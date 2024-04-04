d56e3f756464c8ce70ee3b6ed392c55fda46a5af
Muneeb Ahmad <mahmad.itp@sparkcognition.com>
2024-04-04 06:20:07 +0500

    Adding tf files for AWS instance ALB

---
+++ 03-AWS-ELB-Classic-LoadBalancer/c1-versions.tf
@@ -0,0 +1,24 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
+/*
+Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
+$HOME/.aws/credentials
+*/
